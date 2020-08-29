06671896c714af719e3eb9a6c1d266516339e852
Mario Ciccarelli <mario.ciccarelli@me.com>
2020-08-29 23:14:40 +0200

    Big commit :-(

--- get_vt_int.py
+++ get_vt_int.py
@@ -10,12 +10,9 @@ import pefile
 import struct
 import pandas as pd
 import numpy as np
-import geoviews as gv
-import geoviews.tile_sources as gvts
-from geoviews import dim, opts
-from bokeh.io import output_file, show
 from Crypto.Cipher import ARC4
 from opencage.geocoder import OpenCageGeocode
+from tqdm import tqdm
 
 
 excluded_sections = ['.text', '.rdata', '.data', '.reloc', '.rsrc', '.cfg']
@@ -39,34 +36,11 @@ def decode_sodinokibi_configuration(f):
             enc_len = struct.unpack('I', data[0x24:0x28])[0]
             dec_data = arc4(data[0:32], data[0x28:enc_len + 0x28])
             parsed = json.loads(dec_data[:-1])
-            return str_hash, parsed['pid'], parsed['sub']
+    return str_hash, parsed['pid'], parsed['sub']
             #print("Sample SHA256 Hash: ", str_hash)
             #print("Actor ID: ", parsed['pid'])
             #print("Campaign ID: ", parsed['sub'])
             #print("Attacker's Public Encryption Key: ", parsed['pk']) 
-
-def get_logger():
-    """
-    Configuring the default logger for the project sodinokibiTA
-    """
-    logger = logging.getLogger('sodinokibiTA')
-    logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-    return logger
-
-"""
-def test_logging():
-    logger = get_logger()
-    logger.debug('debug message')
-    logger.info('info message')
-    logger.warning('warn message')
-    logger.error('error message')
-    logger.critical('critical message')
-"""
 
 def get_filename(path):
     items = []
@@ -76,7 +50,6 @@ def get_filename(path):
     return items
 
 def query_vt(sample_list, rp, vt_api_key):
-    logger = get_logger()
     headers = { 'x-apikey' : vt_api_key }
     # Retrieve the list of saved reports
     data_path = get_filename(rp)
@@ -90,7 +63,7 @@ def query_vt(sample_list, rp, vt_api_key):
             file_path = os.path.join(rp, element)
             with open(file_path, 'wb') as f:
                 f.write(response.content)
-                logger.info('Report of %s sample saved' % file_path)
+                print('Report of %s sample saved' % file_path)
 
 def parse_vt_report(vt_reports, rp, sp, gc):
     attacks = []
@@ -103,31 +76,24 @@ def parse_vt_report(vt_reports, rp, sp, gc):
                     if j == 'attributes':
                         city = report["data"][i][j].get('city')
                         country = report["data"][i][j].get('country')
-                        # timestamp = report["data"][i][j].get('date')
+                        timestamp = report["data"][i][j].get('date')
+                        print("[+] Decoding ransomware configuration from sample {}".format(rpt))
                         sample_hash, aID, cID = decode_sodinokibi_configuration(rpt)
                         print("[+] Getting coordinates for city: {} in country {}".format(city, country))
                         result = gc.geocode(city, limit=1, countrycode=country, no_annotation=1)
-                        #result = gc.geocode('?', limit=1, countrycode='KR', no_annotation=1)
                         if not result:
-                            print('Skipping report for city:{}, country:{}'.format(city, country))
+                            print('[-] Coordinates for city:{}, country:{} not found'.format(city, country))
                         else:
                             lat = result[0]['geometry']['lat']
                             lng = result[0]['geometry']['lng']
-                            attack = { 'country': country, 'city': city, 'latitude': lat, 'longitude': lng, 'aid': aID, 'cid': cID, 'hash': sample_hash }
+                            attack = { 'country': country, 'city': city, 'latitude': lat, 'longitude': lng, 'aid': aID, 'cid': cID, 'hash': sample_hash, 'timestamp': timestamp }
                             attacks.append(attack)
     return attacks
 
 def analysis(attacks, pd):
     df = pd.DataFrame(attacks)
     print(df)
-    df.to_csv('data.csv')
-    # output_file("graph.html")
-    # gv.extension('bokeh')
-    # gv_points = gv.Points(df, ['longitude', 'latitude'], ['country', 'city', 'aid', 'cid', 'hash'])
-    # layout = gvts.CartoLight * gv_points
-    # #gvts.CartoLight.options(width=1300, height=800, xaxis=None, yaxis=None, show_grid=False) * gv_points
-    # gv.output(layout)
-  
+    df.to_csv('data.csv', index=False)
 
 # local_datetime_converted = datetime.datetime.fromtimestamp(UTC_datetime_timestamp)
 
@@ -141,7 +107,6 @@ class Main(object):
         self.oc_api_key = args.oc_api_key
         self.samples_path = args.spath
         self.reports_path = args.rpath
-        self.logger = get_logger()
         
         if not self.vt_api_key:
             self.vt_api_key = os.environ.get("VT_API_KEY")
